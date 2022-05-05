91577e779d46fef55cd2cf2bbc30a27bb6fa3e05
Jackson Alves <jackson.emiliano15@gmail.com>
2022-05-05 20:48:08 -0300

    first commit

---
+++ hr-work/src/test/java/com/jkn/hrwork/HrWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkn.hrwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
