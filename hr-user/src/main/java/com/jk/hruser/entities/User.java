cfef4a9b059f4fdf40fc15865922abb11a8ad72e
Jackson Alves <jackson.emiliano15@gmail.com>
2022-05-14 00:33:44 -0300

    User, Role, many to many association

---
+++ hr-user/src/main/java/com/jk/hruser/entities/User.java
@@ -0,0 +1,37 @@
+package com.jk.hruser.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_user")
+public class User implements Serializable {
+
+    private final static long serialVersionUID = 1L ;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_user_role",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+}
